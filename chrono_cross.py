dd92581c1845c3c70641cd314afc630e7c697d0e
Meteor <songge@gmail.com>
2020-05-23 09:16:46 +0800

    Create chrono_cross.py

---
+++ chrono_cross.py
@@ -0,0 +1,19 @@
+# chrono_cross.py
+
+# Themes:
+
+# Features:
+
+import random
+import json
+import time
+from datetime import date, datetime
+from random import *
+from pathlib import Path
+from collections import Counter
+
+def main(): # Main loop about what a player can do in this game.
+    pass
+
+if __name__ == "__main__":
+    main()
