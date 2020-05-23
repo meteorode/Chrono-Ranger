6d1fb1c06c3a858ec98e48310830e311001fffa3
Meteor <songge@gmail.com>
2020-05-23 17:07:37 +0800

    Add class deck()

--- chrono_cross.py
+++ chrono_cross.py
@@ -21,12 +21,37 @@ from collections import Counter
 
 # Define Colors here.
 COLOR_RED = 0
+COLOR_GREEN = 1
+COLOR_BLUE = 2
+COLOR_YELLOW = 3
+COLOR_WHITE = 4
+COLOR_BLACK = 5
 
 class card: # Cards are heroes, events, locations, treasures, etc. Cards have color used for match-3.
     def __init__(self, name, id):
         self.name = name
         self.id = id
         self.color = COLOR_RED
+
+class deck: # Deck is an array of [Cards], will implement some features like draw, discard, remove, etc.
+    def __init__(self): 
+        self.all = []   # All cards in deck
+        self.hands = [] # Hands also equal to battleCommander's hands
+        self.draw_pile = [] # Cards used for drawing.
+        self.discard_pile = [] # Cards discarded came here.
+        self.trash = [] # Cards temporarily removed from battle came here.
+
+    def draw(self, draw_number): # Randomly add [draw_number] cards from draw pile.
+        pass    # _TO_BE_CONTINUED_
+
+    def add_card(self, card): # Add a specific card to hands.
+        pass    # _TO_BE_CONTINUED_
+
+    def discard_card(self, card): # Discard a specific card to discard pile.
+        pass    # _TO_BE_CONTINUED_
+
+    def trash_card(self, card): # temporarily remove a card from battle.
+        pass    # _TO_BE_CONTINUED_
 
 def main(): # Main loop about what a player can do in this game.
     pass
