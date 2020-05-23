53d5d9b6cf12a91283e89d8f3b61d15c3aefea4a
Meteor <songge@gmail.com>
2020-05-23 10:43:36 +0800

    Add features summary.

--- chrono_cross.py
+++ chrono_cross.py
@@ -5,6 +5,11 @@
 #   2.  Leading/witnessing humanity to different destinies by triggering/changing history events.
 
 # Features:
+#   1.  Shuffle/Play your own deck into linear history events.
+#   2.  Click an event card to resolve it
+#   3.  Roll dice to resolve events.
+#   4.  While resolved a card, check special match-3 rules(i.e, [Char, Location, Story] with same color
+#       would trigger new effects).
 
 import random
 import json
